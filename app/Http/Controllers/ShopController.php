d3fa9aece434762bc7528bc53ba0d277ae70fa0c
Luis G <frisneda92@gmail.com>
2022-12-14 23:56:14 -0400

    Backend Web-Shop (index, show): Tests, routes, controller and New trait Inertia with method view custom

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Shop;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ShopController extends Controller
+{
+    public function index()
+    {
+        $shops = Shop::all();
+
+        return $this->view('Shops.Index', compact('shops'));
+    }
+
+    public function show(Shop $shop)
+    {
+        $shop->load('products');
+        return $this->view('Shops.Show', compact('shop'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,8 @@ Route::middleware([
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
+
+Route::group(['prefix'=>'shops', 'as'=>'shops.'], function(){
+    Route::get('/', 'ShopController@index')->name('index');
+    Route::get('/{shop}', 'ShopController@show')->name('show');
+});
