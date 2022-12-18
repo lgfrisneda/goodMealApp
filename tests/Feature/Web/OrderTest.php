b2e1a9e063ba7384616a9ffce1f9d231423f45ee
Luis G <frisneda92@gmail.com>
2022-12-17 21:18:55 -0400

    Tests for web in folder

--- tests/Feature/Web/OrderTest.php
+++ tests/Feature/Web/OrderTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Web;
 
 use App\Models\Order;
 use App\Models\OrderDetail;
--- tests/Feature/Web/ShoppingCartTest.php
+++ tests/Feature/Web/ShoppingCartTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Web;
 
 use App\Models\Product;
 use App\Models\Shop;
